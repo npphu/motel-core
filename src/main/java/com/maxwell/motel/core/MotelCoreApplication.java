7c189c4108ac8bd62c73f65d0074734322bca1dd
Maxwell <npphu97@gmail.com>
2024-04-19 23:12:30 +0700

    first commit

---
+++ src/main/java/com/maxwell/motel/core/MotelCoreApplication.java
@@ -0,0 +1,13 @@
+package com.maxwell.motel.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotelCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxwell/motel/core/MotelCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxwell.motel.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
